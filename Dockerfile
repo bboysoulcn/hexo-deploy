d5366a5438d9a473bd0905c681d08d9031517245
bboysoul <bboysoulcn@gmail.com>
2021-01-04 14:51:26 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Container image that runs your code
+FROM node:14.15.3-alpine
+
+LABEL "com.github.actions.name"="Hexo deploy"
+LABEL "com.github.actions.description"="Auto deploy hexo to github"
+LABEL "com.github.actions.icon"="git-commit"
+LABEL "com.github.actions.color"="orange"
+
+LABEL "repository"="https://github.com/bboysoulcn/hexo-deploy"
+LABEL "homepage"="https://github.com/bboysoulcn/hexo-deploy"
+LABEL "maintainer"="bboysoul"
+
+COPY entrypoint.sh /entrypoint.sh
+
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+
+
+if [ -z "$PUBLISH_DIR" ]
+then
+  echo "You must provide the action with the folder path in the repository where your compiled page generate at, example public."
+  exit 1
+fi
+
+if [ -z "$BRANCH" ]
+then
+  echo "You must provide the action with a branch name it should deploy to, for example master."
+  exit 1
+fi
+
+if [ -z "$PERSONAL_TOKEN" ]
+then
+  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
+  exit 1
+fi
+
+if [ -z "$PUBLISH_REPOSITORY" ]
+then
+  echo "You must provide the git repository address in order to deploy"
+  exit 1
+fi
+
+REPOSITORY_PATH="https://x-access-token:${PERSONAL_TOKEN}@github.com/${PUBLISH_REPOSITORY}.git"
+
+cd $GITHUB_WORKSPACE 
+
+npm install
+
+hexo clean && hexo generate
+
+cd $PUBLISH_DIR
+
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git remote add origin "${REPOSITORY_PATH}"
+
+git add .
+
+datetime=`date '+%Y%m%d-%X'`
+
+git commit -m "update at $datetime"
+
+git push origin "${BRANCH}" -f
+
+echo "finished"
